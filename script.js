edbd746d304334d59ad35a4d1b1995802d592920
oskopis <119396320+oskopis@users.noreply.github.com>
2026-04-21 08:26:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const poga = document.querySelector('.js-poga')
+const bilde = document.querySelector('img')
+
+const pogasTexti = ["Catch me", "Nenoķersi!", "Tu esi zābaks", "Suffer!", "Samaini pelīti, tev nesanāk"]
+let textuIndex = 0
+let movesTillScare = 6
+
+//() => {}
+poga.addEventListener('mouseover', function() {
+    if (movesTillScare === 0){
+        bilde.style.display = "flex"
+    }
+
+    movesTillScare = movesTillScare - 1
+
+    poga.innerHTML = pogasTexti[textuIndex]
+
+    if(textuIndex === pogasTexti.length - 1){
+        textuIndex = 0
+    } else {
+        textuIndex = textuIndex + 1 
+    }
+
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+
+    const buttonWidth = poga.offsetWidth
+    const buttonHeight = poga.offsetHeight
+
+    const maxX = windowWidth - buttonWidth
+    const maxY = windowHeight - buttonHeight
+
+    const randomX = Math.random() * maxX
+    const randomY = Math.random() * maxY
+
+    poga.style.left = randomX + 'px'
+    poga.style.top = `${randomY}px`
+})
